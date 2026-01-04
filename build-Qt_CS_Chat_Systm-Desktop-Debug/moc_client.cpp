59ebf29bb01f3d6e4e43f84cdb6cc1ba960718f9
mari0saad <ms@MSAAD-LT-62.coretech-innovations.com>
2026-01-04 11:47:12 +0200

    Remove bulid files from the tree

--- build-Qt_CS_Chat_Systm-Desktop-Debug/moc_client.cpp
+++ build-Qt_CS_Chat_Systm-Desktop-Debug/moc_client.cpp
@@ -42,16 +42,20 @@ static constexpr auto qt_meta_stringdata_CLASSClientENDCLASS = QtMocHelpers::str
     "connected",
     "",
     "readyRead",
-    "disconnected"
+    "disconnected",
+    "sendLine",
+    "line"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSClientENDCLASS_t {
-    uint offsetsAndSizes[10];
+    uint offsetsAndSizes[14];
     char stringdata0[7];
     char stringdata1[10];
     char stringdata2[1];
     char stringdata3[10];
     char stringdata4[13];
+    char stringdata5[9];
+    char stringdata6[5];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSClientENDCLASS_t::offsetsAndSizes) + ofs), len 
@@ -61,13 +65,17 @@ Q_CONSTINIT static const qt_meta_stringdata_CLASSClientENDCLASS_t qt_meta_string
         QT_MOC_LITERAL(7, 9),  // "connected"
         QT_MOC_LITERAL(17, 0),  // ""
         QT_MOC_LITERAL(18, 9),  // "readyRead"
-        QT_MOC_LITERAL(28, 12)   // "disconnected"
+        QT_MOC_LITERAL(28, 12),  // "disconnected"
+        QT_MOC_LITERAL(41, 8),  // "sendLine"
+        QT_MOC_LITERAL(50, 4)   // "line"
     },
     "Client",
     "connected",
     "",
     "readyRead",
-    "disconnected"
+    "disconnected",
+    "sendLine",
+    "line"
 };
 #undef QT_MOC_LITERAL
 #endif // !QT_MOC_HAS_STRING_DATA
@@ -79,7 +87,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSClientENDCLASS[] = {
       11,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -87,14 +95,16 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSClientENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   32,    2, 0x08,    1 /* Private */,
-       3,    0,   33,    2, 0x08,    2 /* Private */,
-       4,    0,   34,    2, 0x08,    3 /* Private */,
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    0,   39,    2, 0x08,    2 /* Private */,
+       4,    0,   40,    2, 0x08,    3 /* Private */,
+       5,    1,   41,    2, 0x0a,    4 /* Public */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    6,
 
        0        // eod
 };
@@ -113,7 +123,10 @@ Q_CONSTINIT const QMetaObject Client::staticMetaObject = { {
         // method 'readyRead'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'disconnected'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'sendLine'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>
     >,
     nullptr
 } };
@@ -127,10 +140,10 @@ void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         case 0: _t->connected(); break;
         case 1: _t->readyRead(); break;
         case 2: _t->disconnected(); break;
+        case 3: _t->sendLine((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
         default: ;
         }
     }
-    (void)_a;
 }
 
 const QMetaObject *Client::metaObject() const
@@ -152,13 +165,13 @@ int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
